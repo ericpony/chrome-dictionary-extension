7bbc3202872bfba2d1b513361b463ac221ea23a7
ericpony <ericpony@csie.org>
2016-09-27 12:30:48 +0800

    fix links and add tags

--- js/dictionaries/urban.js
+++ js/dictionaries/urban.js
@@ -2,7 +2,7 @@ DICTIONARIES.urban = {
   title: 'Urban Dictionary',
   desc: '英英',
   langs: ['en'],
-  title2: 'Urban Dictionary',
+  text: 'Urban Dictionary',
   query: function (q, response) {
     var self = this;
     $.get('http://www.urbandictionary.com/define.php?term=' + q).done(function (data) {
@@ -11,14 +11,16 @@ DICTIONARIES.urban = {
         var word = $(element).find('.word').text();
         var meaning = $(element).find('.meaning').html();
         var example = $(element).find('.example').html();
+        var tags = $(element).find('.tags').html();
         result += '<div class="panel panel-info">\
           <div class="panel-heading">\
             <h3 class="panel-title">' + word + '</h3>\
           </div>\
-          <div class="panel-body">' + meaning + '</div>\
+          <div class="panel-body">' + meaning + '<br />' + tags + '</div>\
           <div class="panel-footer"><h5>Example</h5>' + example + '</div>\
         </div>';
       });
+      result = result.replace(/href="\//g, 'href="http://www.urbandictionary.com/');
       response(self, result);
     });
   }
